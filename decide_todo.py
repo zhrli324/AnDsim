ba2e3dee65aa252666288f0cd177238a579dc2c1
ailor <zhrli324@gmail.com>
2024-09-11 02:20:02 +0800

    Completed some modules

---
+++ decide_todo.py
@@ -0,0 +1,21 @@
+
+
+class Thought:
+    """
+
+    """
+    def __init__(
+            self,
+    ) -> None:
+        self.obj_name = None
+        self.content = None
+        pass
+
+
+def execute(thought):
+    """
+    执行操作
+    :param thought:
+    :return:
+    """
+    pass
